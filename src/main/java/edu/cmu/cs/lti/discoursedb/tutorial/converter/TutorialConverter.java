5657b5a6fafd7004cc73be1927831525acaeff7c
Oliver Ferschke <oliver.ferschke@gmail.com>
2016-08-24 13:48:31 -0400

    Removed Order annotation

--- src/main/java/edu/cmu/cs/lti/discoursedb/tutorial/converter/TutorialConverter.java
+++ src/main/java/edu/cmu/cs/lti/discoursedb/tutorial/converter/TutorialConverter.java
@@ -25,15 +25,10 @@ import edu.cmu.cs.lti.discoursedb.tutorial.model.GitHubIssueComment;
 /**
  * This component will be discovered by the starter class <code>TutorialConverterApplication</code>.<br/>
  * Since this class implements CommandLineRuner, the <code>run</code> method will receive the args of the main method of the starter class.<br/>
- * 
- * The Order annotations is not necessary. It allows to specify the order of execution in case we have multiple components.
- * This class can directly access any DiscourseDB repositories and services by Autowiring them, but it is recommended to wrap all interactions in methods located in teh <code>TutorialConverterSetrvice</code> class, which is already autowired in this stub.
- * The main reason for this is that all calls to service methods will run in separate transactions.
- * 
+ *
  * @author Oliver Ferschke
  */
 @Component
-@Order(1)
 @SuppressWarnings("unused")
 public class TutorialConverter implements CommandLineRunner {
 
